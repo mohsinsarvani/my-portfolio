f33db084891665e84df9d3bbbf49860fccae989d
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-06-04 10:16:08 +0100

    updated footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,11 +10,21 @@ const Footer = () => (
           </a>
         </li>
         <li>
+          <a href="#" className="fa fa-linkedin">
+            <span className="label">Linkedin</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="fa-stack-overflow">
+            <span className="label">Stack Overflow</span>
+          </a>
+        </li>
+        <li>
           <a href="#" className="icon fa-github">
             <span className="label">Github</span>
           </a>
         </li>
-        
+
         <li>
           <a href="#" className="icon fa-envelope-o">
             <span className="label">Email</span>
