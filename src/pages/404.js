3fdfd183c6e79dcf55d323a08e54fbd27ee3abd8
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-06-05 23:34:52 +0100

    deleted some component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,10 @@
 import React from "react"
 
 import LayoutWrapper from "../components/LayoutWrapper/LayoutWrapper"
-import SEO from "../components/seo"
+
 
 const NotFoundPage = () => (
   <LayoutWrapper>
-    <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just higggt a route that doesn&#39;t exist... the sadness.</p>
   </LayoutWrapper>
