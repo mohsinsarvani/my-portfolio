aa936be9e92924842dc8835d0591dfc201ca54de
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-06-06 22:47:52 +0100

    end of the batle

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './SocialMedia.css'
-
+import { SocialIcon } from 'react-social-icons';
 const SocialMedia = () => {
 
     const address = "South East,London";
@@ -11,42 +11,14 @@ const SocialMedia = () => {
 
     return (
         <div id="media">
-            <ul>
-                <li>
-                    <a href="https://twitter.com/ricki_me" className="icon fa-twitter">
-                        <span className="label">Twitter</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="https://linkedin.com/in/ricardo-josé-vargas-leslie-235747143" className="fa fa-linkedin">
-                        <span className="label">Linkedin</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="https://stackoverflow.com/users/4814583/ricki" className="fa-stack-overflow">
-                        <span className="label">Stack Overflow</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="https://github.com/RicardoVargasLeslie" className="icon fa-github">
-                        <span className="label">Github</span>
-                    </a>
-                </li>
+            <ul className="icons">
 
-                <li>
-                    <a href="#" className="icon fa-envelope-o">
-                        <span className="label">Email</span>
-                    </a>
-                </li>
+                <li><SocialIcon url="" network="github" bgColor="#000000" style={{ height: 25, width: 25 }} /></li>
+                <li> <SocialIcon url="http://linkedin.com/in/jaketrent" style={{ height: 25, width: 25 }} /></li>
+                <li><SocialIcon url="" network="twitter" bgColor="#49a1eb" style={{ height: 25, width: 25 }} /></li>
+
             </ul>
-            <ul className="labeled-icons">
-                <li>{name}</li>
-                <li className="icon fa-home" >{address}</li>
-                <li className="icon fa-mobile">{phone}</li>
-                <li className="icon fa-envelope-o">{email}</li>
-                <li className="copyright"> © All rights are reserved | {new Date().getFullYear()} |</li>
-                <li aria-label="love" role="img"> </li>
-            </ul>
+
         </div>
     )
 }
