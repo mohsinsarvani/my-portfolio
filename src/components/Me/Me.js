696b5f76947d92645e2056c01a0ff5d027c3db04
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-06-07 03:48:01 +0100

    buen trabajo

--- src/components/Me/Me.js
+++ src/components/Me/Me.js
@@ -3,14 +3,16 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 import Button from '@material-ui/core/Button'
 import code from '../../assets/images/back2.jpg'
 import './Me.css'
+import Divider from '@material-ui/core/Divider'
 
 
 const Me = () => {
     return (
+       
         <div id="main">
-            
+             <Divider />
             <section className="iam" >
-                <p>
+              
                     <strong>
                         Just in case you don't understand, don't worry
                         I will explain.
@@ -19,10 +21,12 @@ const Me = () => {
                         to a machine to perform a task such as you may be seeing this message on the screen
                         cool don't you think soo? :)
                    </strong>
-                </p>
-                <Button>Hire Me</Button>
+                
+                <Button  variant="outlined" color="primary" size="medium" >Hire Me</Button>
             </section >
+            <Divider />
         </div>
+        
     )
 }
 
