18ed23ac3851a1afb516aba8fca8f1773b22059f
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-06-11 00:32:39 +0100

    change name

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -9,7 +9,7 @@ function Avatar() {
                 <li>
                     <div class="rp-item rp-img-1">
                         <div class="rp-info">
-                            <h3>Ricardo Vargas</h3>
+                            <h3>Ricardo V</h3>
                         </div>
                     </div>
                 </li>
